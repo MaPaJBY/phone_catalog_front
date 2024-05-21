06d1b33397b3cf74d2ef4c56603096c456134c43
Paweł Kloc <pawelkloc89@gmail.com>
2024-05-21 15:47:01 +0200

    temp fix 2

--- src/context/ProductsContext.tsx
+++ src/context/ProductsContext.tsx
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-import React, { createContext, FC, useContext, useEffect, useState } from 'react';
-import { IProductDetails } from '../types';
-=======
 import React, {
   createContext,
   FC,
@@ -10,18 +6,15 @@ import React, {
   useState,
 } from 'react';
 import { ICartProduct, IProductDetails } from '../types';
->>>>>>> Stashed changes
 
 interface IProductsContext {
   phones: IProductDetails[];
 }
 
 export const ProductsContext = createContext<IProductsContext>({
-  phones:[]
+  phones: [],
 });
 
-<<<<<<< Updated upstream
-=======
 interface ICartItem {
   count: number;
   product: ICartProduct;
@@ -30,24 +23,20 @@ type CartItems = {
   [key: string]: ICartItem;
 };
 
->>>>>>> Stashed changes
 interface Props {
   children: React.ReactNode;
 }
 
 export const ProductsProvider: FC<Props> = ({ children }) => {
-  const [phones, setPhones] = useState<IProductDetails[]>([])
+  const [phones, setPhones] = useState<IProductDetails[]>([]);
   const [_loading, setLoading] = useState(true);
   const [_error, setError] = useState(null);
 
-<<<<<<< Updated upstream
-=======
   const [cartItems, setCartItems] = useState<CartItems>(() => {
     const item = localStorage.getItem(CART_STORAGE_KEY);
 
     return item ? JSON.parse(item) : {};
   });
->>>>>>> Stashed changes
 
   useEffect(() => {
     fetch('/api/phones.json')
@@ -61,7 +50,7 @@ export const ProductsProvider: FC<Props> = ({ children }) => {
       .then(data => {
         setPhones(data);
         setLoading(false);
-        console.log(data)
+        console.log(data);
       })
       .catch(error => {
         setError(error);
@@ -69,17 +58,14 @@ export const ProductsProvider: FC<Props> = ({ children }) => {
       });
   }, []);
 
-<<<<<<< Updated upstream
-=======
   useEffect(() => {
     localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(cartItems));
   }, [cartItems]);
 
->>>>>>> Stashed changes
   return (
     <ProductsContext.Provider
       value={{
-        phones
+        phones,
       }}
     >
       {children}
