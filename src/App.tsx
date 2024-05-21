dca71fd65f6489de9da8f15798c3d55631cfed1c
Paweł Kloc <pawelkloc89@gmail.com>
2024-05-21 11:23:15 +0200

    Added NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.scss';
 import Home from './components/Home/Home';
 import Layout from './components/Layout/Layout';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
 export const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        {/* <Route path="*" element={<ErrorPage />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   </Router>
