7bc0986ec96932c4003b9b2853f116c825bc2675
Maryna Karpova <mari.artarea@gmail.com>
2024-05-30 11:53:36 +0200

    sort by, items per page

--- src/types.ts
+++ src/types.ts
@@ -23,6 +23,7 @@ export interface IProductDetails {
   camera: string;
   zoom: string;
   cell: string[];
+  year: number;
 }
 
 export interface ICartProduct {
@@ -71,5 +72,59 @@ export enum SortType {
   ZA = 'za',
   LOW_TO_HIGH = 'lowToHigh',
   HIGH_TO_LOW = 'highToLow',
-  ALL = 'All',
+  WITHOUT_SORT = 'without-sort',
+  SHOW_ALL = 'show-all',
+  NEWEST_TO_OLDEST = 'newest-to-oldest',
+  OLDEST_TO_NEWEST = 'oldest-to-newest',
 }
+
+export const SHOW_ALL_PRODUCTS = 'show-all-products';
+
+export const convertToProductDetails = (product: ProductT): IProductDetails => {
+  return {
+    id: product.itemId,
+    namespaceId: '',
+    name: product.name,
+    category: product.category,
+    capacityAvailable: [product.capacity],
+    capacity: product.capacity,
+    priceRegular: product.fullPrice,
+    priceDiscount: product.price,
+    colorsAvailable: [product.color],
+    color: product.color,
+    images: [product.image],
+    description: [],
+    screen: product.screen,
+    resolution: '',
+    processor: '',
+    ram: product.ram,
+    camera: '',
+    zoom: '',
+    cell: [],
+    year: product.year,
+  };
+};
+
+export const convertToProductT = (product: IProductDetails): ProductT => {
+  const category =
+    product.category === 'phones' ||
+    product.category === 'tablets' ||
+    product.category === 'accessories'
+      ? product.category
+      : 'phones';
+
+  return {
+    id: parseInt(product.id),
+    category,
+    itemId: product.id,
+    name: product.name,
+    fullPrice: product.priceRegular,
+    price: product.priceDiscount,
+    screen: product.screen,
+    capacity: product.capacity,
+    color: product.color,
+    ram: product.ram,
+    year: 0,
+    image: product.images[0],
+  };
+};
