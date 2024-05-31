78fa0cf6422010139a5f825bf86a53af1e16b0d5
Paweł Kloc <pawelkloc89@gmail.com>
2024-05-31 13:40:05 +0200

    burger menu fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,65 +1,151 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Icon from '../Icon/Icon';
 import { Icons } from '../../types';
 import CartBadge from '../Badge/Badge';
-import FavoriteBadge from '../FavoriteBadge/FavoriteBadge';
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(prev => !prev);
+    if (isMenuOpen) {
+      navigate('/');
+    }
+  };
+
   return (
-    <header className={styles.header}>
-      <img className={styles.divLogo} src={'/img/logo.png'} alt="logo" />
-      <nav className={styles.navbar}>
-        <ul className={styles.navlinks}>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? styles.active : '')}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/phones"
-              className={({ isActive }) => (isActive ? styles.active : '')}
-            >
-              Phones
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/tablets"
-              className={({ isActive }) => (isActive ? styles.active : '')}
-            >
-              Tablets
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/accessories"
-              className={({ isActive }) => (isActive ? styles.active : '')}
-            >
-              Accessories
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.icons}>
-        <NavLink to="/favorites">
-          <Icon iconId={Icons.HEART} className={styles.heart} />
-          <FavoriteBadge />
-        </NavLink>
-        <NavLink to="/cart">
-          <Icon iconId={Icons.CART} className={styles.cart} />
-          <CartBadge />
-        </NavLink>
-        <div className={styles.union}>
-          <Icon iconId={Icons.BURGER} className={styles.burger} />
+    <>
+      <header className={styles.header}>
+        <img className={styles.divLogo} src={'/img/logo.png'} alt="logo" />
+        <nav className={`${styles.navbar} ${isMenuOpen ? styles.open : ''}`}>
+          <ul className={styles.navlinks}>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/phones"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+              >
+                Phones
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/tablets"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+              >
+                Tablets
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/accessories"
+                className={({ isActive }) => (isActive ? styles.active : '')}
+              >
+                Accessories
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.icons}>
+          <NavLink to="/favorites">
+            <Icon iconId={Icons.HEART} className={styles.heart} />
+          </NavLink>
+          <NavLink to="/cart">
+            <Icon iconId={Icons.CART} className={styles.cart} />
+            <CartBadge />
+          </NavLink>
+          <div className={styles.union} onClick={toggleMenu}>
+            <Icon
+              iconId={isMenuOpen ? Icons.CLOSE : Icons.BURGER}
+              className={styles.burger}
+            />
+          </div>
         </div>
+      </header>
+      <div className={`${styles.overlay} ${isMenuOpen ? styles.open : ''}`}>
+        {isMenuOpen && (
+          <>
+            <div className={styles.overlay_header}>
+              <img
+                className={styles.divLogo}
+                src={'/img/Logo.png'}
+                alt="logo"
+              />
+              <div className={styles.close} onClick={toggleMenu}>
+                <Icon iconId={Icons.CLOSE} />
+              </div>
+            </div>
+            <nav className={styles.overlayNav}>
+              <ul className={styles.navLinksBurger}>
+                <li>
+                  <NavLink
+                    to="/"
+                    onClick={toggleMenu}
+                    className={({ isActive }) =>
+                      isActive ? styles.active : ''
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/phones"
+                    onClick={toggleMenu}
+                    className={({ isActive }) =>
+                      isActive ? styles.active : ''
+                    }
+                  >
+                    Phones
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/tablets"
+                    onClick={toggleMenu}
+                    className={({ isActive }) =>
+                      isActive ? styles.active : ''
+                    }
+                  >
+                    Tablets
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/accessories"
+                    onClick={toggleMenu}
+                    className={({ isActive }) =>
+                      isActive ? styles.active : ''
+                    }
+                  >
+                    Accessories
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            <div className={styles.icons}>
+              <div className={styles.heartContainer}>
+                <Icon iconId={Icons.HEART} className={styles.heart} />
+              </div>
+              <div className={styles.cartContainer}>
+                <Icon iconId={Icons.CART} className={styles.cart} />
+              </div>
+            </div>
+          </>
+        )}
       </div>
-    </header>
+    </>
   );
 };
 
